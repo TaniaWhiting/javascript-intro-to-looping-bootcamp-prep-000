9dc8cca91068c512bb654e357a03b64a6ba7aaf6
Tania Whiting <tanwhiting@aol.com>
2018-08-16 16:49:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,3 +8,12 @@ function forLoop(array) {
   }
   return array;
 }
+
+function whileLoop(number) {
+  let countdown = number;
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown--;
+  }
+  return "done";
+}
